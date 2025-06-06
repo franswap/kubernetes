d04e6ad41261cb5ff3d4f6ea1175ddd38aef8e80
Etienne <etiennewazer@gmail.com>
2025-06-06 16:26:45 +0200

    Amélioration du Dockerfile et docker-compose

--- app/php/www/connect.php
+++ app/php/www/connect.php
@@ -1,12 +1,12 @@
 <?php
-    $host = getenv('MYSQL_HOST');
-    $username = getenv('MYSQL_USER');
-    $password = getenv('MYSQL_PASSWORD');
-    $db = getenv('MYSQL_DATABASE');
+    $host = getenv('DB_HOST');
+    $db = getenv('DB_NAME');
+    $username = getenv('DB_USER');
+    $password = getenv('DB_PASSWORD');
 
     // Connexion avec pdo mysql
     $db = new PDO("mysql:host=$host;dbname=$db", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    
+
 ?>
